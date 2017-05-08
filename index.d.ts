35bb9d3d24d898ac6a6455fab6c24bbd75af3056
Blake Embrey <hello@blakeembrey.com>
2017-05-08 14:55:59 -0400

    Add `default` option to definition

--- index.d.ts
+++ index.d.ts
@@ -3,10 +3,13 @@ declare function yn (value: string | number | boolean, options?: yn.Options): bo
 declare namespace yn {
   export interface Options {
     /**
-     * Lenient mode will use a key distance-based score
-     * to leniently accept typos of "yes" and "no".
+     * Use a key distance-based score to leniently accept typos of `yes` and `no`.
      */
     lenient?: boolean;
+    /**
+     * Default value if no match was found.
+     */
+    default?: boolean | null;
   }
 }
 
